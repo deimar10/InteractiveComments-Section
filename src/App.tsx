f0148ae7cbcb9e390db507ba0606fff2aeaaf37e
Deimar <deimarparn14@gmail.com>
2023-03-23 14:10:11 +0200

    Created comments interface

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
+import { CommentsInterface } from './Interfaces/Interface';
 import Home from './Pages/Home';
 
 function App() {
-  const [comments, setComments] = useState<any>();
-  
+  const [comments, setComments] = useState<CommentsInterface[]>();
+
   useEffect(() => {
     axios.get('http://localhost:3002/comments/get')
       .then(response => {
---
+++ src/Interfaces/Interface.ts
@@ -0,0 +1,7 @@
+export interface CommentsInterface {
+    id: number;
+    content: string;
+    createdAt: Date;
+    score: number;
+    username: string;
+}
