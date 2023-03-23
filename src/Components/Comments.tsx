cb5fc2351f30512deca1fe60f7522fa7c3cac3cd
Deimar <deimarparn14@gmail.com>
2023-03-23 13:14:53 +0200

    Created project structure

---
+++ src/Components/Comments.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Comments.scss';
+
+function Comments() {
+    return (
+        <div className='comments-main-section'>
+            
+        </div>
+    );
+}
+
+export default Comments;
---
+++ src/Pages/Home.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Home.scss';
+
+function Home() {
+    return (
+        <div className='main-comments-container'>
+            
+        </div>
+    );
+}
+
+export default Home;
