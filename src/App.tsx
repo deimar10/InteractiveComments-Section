2146a3a0ede7aca79e4615c836ef3ae1bbd0c7fc
Deimar <deimarparn14@gmail.com>
2023-03-23 14:02:41 +0200

    Implemented axios get comments request

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,17 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 import Home from './Pages/Home';
 
 function App() {
+  const [comments, setComments] = useState<any>();
+  
+  useEffect(() => {
+    axios.get('http://localhost:3002/comments/get')
+      .then(response => {
+        const data = response.data;
+        setComments(data);
+      })
+  },[]);
   return (
     <div className="App">
      <Home />
